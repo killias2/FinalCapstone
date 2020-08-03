ca9a6057fddf5fba2ceb9fa6a43f5e24f652db53
Matt Stavinga <mattstavinga@gmail.com>
2020-08-03 16:37:35 -0400

    changed styling for login & create user

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -31,8 +31,11 @@
         v-model="user.password"
         required
       />
-      <router-link :to="{ name: 'register' }">Need an account?</router-link>
+
       <button type="submit">Sign in</button>
+      <div >
+        <router-link class="register" :to="{ name: 'register' }">Need an account?</router-link>
+      </div>
     </form>
   </div>
 </template>
@@ -74,3 +77,71 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+
+  h1 {
+    height: 100px;
+    width: 100%;
+    font-size: 45px;
+    text-align: center;
+    background: rgb(28, 143, 158);
+    color: white;
+    line-height: 150%;
+    border-radius: 3px 3px 0 0;
+    box-shadow: 0 2px 5px 1px rgba(0,0,0,0.2);
+    box-sizing: border-box;
+    padding: 20px;
+  }
+  form {
+    box-sizing: border-box;
+    width: 500px;
+    margin: auto;
+    box-shadow: 2px 2px 5px 1px rgba(0,0,0,0.2);
+    padding-bottom: 40px;
+    border-radius: 3px;
+  }
+  input {
+    margin: 25px 25px;
+    width: 70%;
+    display: block;
+    border: none;
+    padding: 10px 0;
+    border-bottom: solid 1px;
+    transition: all 0.3s cubic-bezier(.64,.09,.08,1);
+    background: linear-gradient(to bottom, rgba(255,255,255,0) 96%, color 4%);
+    background-position: -200px 0;
+    background-size: 200px 100%;
+    background-repeat: no-repeat;
+    color: rgb(28, 143, 158);
+  }
+  button {
+    border: none;
+    background: rgb(28, 143, 158);
+    cursor: pointer;
+    border-radius: 3px;
+    padding: 6px;
+    width: 200px;
+    color: white;
+    margin-left: 25px;
+    margin-right: 25px;
+    margin-bottom: 25px;
+    box-shadow: 0 3px 6px 0 rgba(0,0,0,0.2);
+  }
+  button:hover {
+    transform: translateY(-3px);
+    box-shadow: 0 6px 6px 0 rgba(0,0,0,0.2);
+  }
+  .sr-only {
+    margin-left: 25px;
+    font-weight: bold;
+  }
+  .register {
+    margin-left: 25px;
+    color: blue;
+  }
+  .register:hover {
+    color: blueviolet;
+  }
+
+</style>
