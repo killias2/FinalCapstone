681bf52b21ca84ad142fd2551291c2dcf0e367e9
Shane Killian <killias2@yahoo.com>
2020-08-04 17:05:19 -0400

    Added test methods for Bracket Type to Trace a SQL Error

--- backend/src/main/java/com/techelevator/controller/TournamentController.java
+++ backend/src/main/java/com/techelevator/controller/TournamentController.java
@@ -3,6 +3,7 @@ package com.techelevator.controller;
 import org.springframework.web.bind.annotation.*;
 
 import com.techelevator.dao.TournamentDAO;
+import com.techelevator.model.BracketType;
 import com.techelevator.model.Tournament;
 
 @RestController
@@ -15,6 +16,16 @@ public class TournamentController {
 	
 	@RequestMapping(value = "/tournaments", method = RequestMethod.POST)
 	public boolean createTournament(@RequestBody Tournament tournament) {
-		return this.tournamentDAO.createTournament(tournament);
+		return tournamentDAO.createTournament(tournament);
+	}
+	
+	@RequestMapping(value = "/brackettype", method = RequestMethod.POST)
+	public boolean createBracketType(@RequestBody BracketType bracketType) {
+		return tournamentDAO.createBracketType(bracketType);
+	}
+	
+	@RequestMapping(value = "/brackettype", method = RequestMethod.GET)
+	public BracketType getBracketType() {
+		return tournamentDAO.getBracketType();
 	}
 }
--- backend/src/main/java/com/techelevator/dao/TournamentSqlDAO.java
+++ backend/src/main/java/com/techelevator/dao/TournamentSqlDAO.java
@@ -1,8 +1,10 @@
 package com.techelevator.dao;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Service;
 
+import com.techelevator.model.BracketType;
 import com.techelevator.model.Tournament;
 
 @Service
@@ -12,6 +14,7 @@ public class TournamentSqlDAO implements TournamentDAO{
 	public TournamentSqlDAO(JdbcTemplate jdbcTemplate) {
 		this.jdbcTemplate = jdbcTemplate;
 	}
+	
 	@Override
 	public boolean createTournament(Tournament tournament) {
 		String sql = "INSERT INTO tournaments (t_name, is_open, gameid, bracketid, organizerid, start_date, end_date, is_seeded, is_complete)"
@@ -21,6 +24,25 @@ public class TournamentSqlDAO implements TournamentDAO{
 		
 		// TODO Auto-generated method stub
 		
+	}
+	
+	@Override
+	public boolean createBracketType(BracketType bracketType) {
+		String sql = "INSERT INTO bracket_types (bracket_name) VALUES (?)";
+		jdbcTemplate.update(sql, bracketType.getBracketName());
+		return true;
+	}
+	
+	@Override
+	public BracketType getBracketType() {
+		String sql = "SELECT bracketid, bracket_name FROM bracket_types";
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
+		BracketType returnedBracketType = new BracketType();
+		if (results.next()){
+			returnedBracketType.setBracketName(results.getString("bracket_name"));
+			returnedBracketType.setBracketId(results.getLong("bracketid"));
+		}
+		return returnedBracketType;
 	}
 
 }
