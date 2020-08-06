c17486169f1e2e40819b1de2c6c0f4fac2ac5047
Shane Killian <killias2@yahoo.com>
2020-08-06 14:31:54 -0400

    Added Basic Email Capability and Random Password Generation

--- backend/src/main/java/com/techelevator/controller/EmailController.java
+++ backend/src/main/java/com/techelevator/controller/EmailController.java
@@ -1,20 +1,66 @@
 package com.techelevator.controller;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.techelevator.dao.UserDAO;
 import com.techelevator.email.EmailService;
+import com.techelevator.model.PasswordRequest;
 
 @RestController
 public class EmailController {
+	
+	String emailBody = "";
+    private UserDAO userDAO;
  
     @Autowired
     private EmailService emailService;
+    
+    public EmailController(UserDAO userDAO) {
+    	this.userDAO = userDAO;
+    }
  
-    @GetMapping(value = "/sendmail")
-    public String sendmail() {
-    	emailService.sendMail("killias2@yahoo.com", "Test", "Test");
+    @RequestMapping(value = "/sendmail", method = RequestMethod.POST)
+    public String sendmail(@RequestBody PasswordRequest pwR) {
+    	emailBody = bodyBuilder();
+    	emailService.sendMail(pwR.getEmailAddress(), "Your Temporary Password", emailBody);
     	return "EmailSent";
     }
+    
+    private String bodyBuilder() {
+    	String newBody = "We have received a lost password request for your account and have generated a new password. The new " +
+    			"password is: ";
+    	newBody += generateCommonLangPassword();
+    	return newBody;
+    }
+    
+    public String generateCommonLangPassword() {
+        String upperCaseLetters = RandomStringUtils.random(2, 65, 90, true, true);
+        String lowerCaseLetters = RandomStringUtils.random(2, 97, 122, true, true);
+        String numbers = RandomStringUtils.randomNumeric(2);
+        String specialChar = RandomStringUtils.random(2, 33, 47, false, false);
+        String totalChars = RandomStringUtils.randomAlphanumeric(2);
+        String combinedChars = upperCaseLetters.concat(lowerCaseLetters)
+          .concat(numbers)
+          .concat(specialChar)
+          .concat(totalChars);
+        List<Character> pwdChars = combinedChars.chars()
+          .mapToObj(c -> (char) c)
+          .collect(Collectors.toList());
+        Collections.shuffle(pwdChars);
+        String password = pwdChars.stream()
+          .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append)
+          .toString();
+        return password;
+    }
+    
 }
