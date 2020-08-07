bf284ad992773912e9b5941812d8c8d2ee757115
Katelyn Shick <kt.christine7@gmail.com>
2020-08-07 14:44:56 -0400

    Commit

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,13 +7,13 @@ import router from './router/index'
 import store from './store/index'
 import axios from 'axios'
 // import vuetify from './plugins/vuetify'
-import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+// import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+// import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-Vue.use(BootstrapVue)
-Vue.use(BootstrapVueIcons)
+// Vue.use(BootstrapVue)
+// Vue.use(BootstrapVueIcons)
 
 Vue.config.productionTip = false
 
