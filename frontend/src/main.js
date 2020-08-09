7b1633f8cb19070a9250d8acba70c7d53dc65c70
Ryan Stanley <Stanman519@gmail.com>
2020-08-09 19:24:18 -0400

    finished matt merge

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,19 +7,11 @@ import router from './router/index'
 import store from './store/index'
 import axios from 'axios'
 // import vuetify from './plugins/vuetify'
-<<<<<<< HEAD
-// import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
-
-// import 'bootstrap/dist/css/bootstrap.css'
-// import 'bootstrap-vue/dist/bootstrap-vue.css'
-
-=======
 //import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 import VueSimpleAlert from 'vue-simple-alert';
 // import 'bootstrap/dist/css/bootstrap.css'
 // import 'bootstrap-vue/dist/bootstrap-vue.css'
 Vue.use(VueSimpleAlert);
->>>>>>> b46f31c5aa99111ca9b476906ccbe7c5725e82b4
 // Vue.use(BootstrapVue)
 // Vue.use(BootstrapVueIcons)
 
