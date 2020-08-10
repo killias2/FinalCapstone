cc7a8b2b442c7c37f7cb5c38a3e7e0b8dbf6ae76
Shane Killian <killias2@yahoo.com>
2020-08-10 07:48:37 -0400

    Started Katelyn merge?

---
+++ frontend/src/components/CurrentTournaments.vue
@@ -0,0 +1,61 @@
+<template>
+    <div>
+        <h1>Current Tournaments</h1>
+                    <div class="tournament" v-for="tournament in tournaments" v-bind:key="tournament.id" 
+        v-bind:currentTournament="tournament" v-on:click="viewTournamentDetails(tournament)">
+            <h2>{{tournament.tournamentName}}</h2>
+            <h3>{{tournament.startDate}} to {{tournament.endDate}}</h3>
+            <h3>{{tournament.numberOfTeams}} teams</h3>
+        </div>
+    </div>
+</template>
+
+<script>
+import TournamentService from '../services/TournamentService';
+
+export default {
+    data() {
+        return {
+            tournaments: []
+        };
+    },
+    methods: {
+        viewTournamentDetails(tournament) {
+            this.$store.commit('SET_CURRENT_TOURNAMENT', {
+            saveMe: tournament
+            })
+            this.$router.push(`/tournaments/${tournament.id}`);
+        }
+    },
+    created() {
+        TournamentService.getCurrentTournaments().then(response => {
+           this.tournaments = response.data;
+        })
+    }
+}
+</script>
+
+<style scoped>
+    .tournament {
+        
+        
+        display: block;
+        text-align: center;
+        color: white;
+        font-weight: bolder;
+        text-shadow: 2px;
+        font-size: 20px;
+        margin-left: 300px;
+        margin-right: 300px;
+        background-color: rgb(8, 69, 97);
+    }
+    h2 {
+        font-size: 30px;
+    }
+    h3 {
+        font-size: 18px;
+    }
+    .tournament:hover {
+        background: blueviolet;
+    }
+</style>
---
+++ frontend/src/views/PastTournaments.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <sidebar />
+        <past-tournaments />
+        <user-nav />
+    </div>
+</template>
+
+<script>
+import PastTournaments from "../components/PastTournaments"
+import Sidebar from '../components/Sidebar'
+import UserNav from '../components/UserNav'
+
+export default {
+    components: {
+        PastTournaments,
+        Sidebar,
+        UserNav
+    }
+};
+</script>
